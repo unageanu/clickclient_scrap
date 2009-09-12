ee374bb7ea72cf945ca6e8669cc443afbb9ea9a1
unageanu <y-masaya@red.hot.co.jp>
2009-09-12 10:48:40 +0900

    ClickClient→ClickClientScrapへの変更漏れがあったため、修正。

--- sample/sample_market_order.rb
+++ sample/sample_market_order.rb
@@ -9,6 +9,6 @@ c = ClickClientScrap::Client.new
 c.fx_session( USER, PASS ) {|session|
 
   #成り行き注文
-  session.order( ClickClient::FX::USDJPY, ClickClient::FX::BUY, 1)
-  session.order( ClickClient::FX::USDJPY, ClickClient::FX::SELL, 1)
+  session.order( ClickClientScrap::FX::USDJPY, ClickClientScrap::FX::BUY, 1)
+  session.order( ClickClientScrap::FX::USDJPY, ClickClientScrap::FX::SELL, 1)
 }
